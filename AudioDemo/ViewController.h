c54ad0514d98331dd064e035708683c51a96c42f
chbuxing1011 <chbuxing@163.com>
2015-07-01 11:15:14 +0800

    创建音频录制、播放工程

---
+++ AudioDemo/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  AudioDemo
+//
+//  Created by tusm on 15/2/14.
+//  Copyright (c) 2015年 tusm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+/**
+ *  音乐录制、播放
+ */
+@interface ViewController : UIViewController<AVAudioRecorderDelegate>
+
+
+@end
+
